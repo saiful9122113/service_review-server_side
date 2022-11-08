b0b60c3540243dca025c4e7f3920d6df26da1b91
Md. Saiful Islam <saiful9122113@gmail.com>
2022-11-09 02:28:10 +0600

    services data added in database & some others

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 const app = express();
 const port = process.env.port || 5000;
 
 // middle wares
 app.use(cors());
 app.use(express.json());
+
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lbe4zil.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const serviceCollection = client.db('serviceReviewer').collection('services');
+    }
+    finally{
+
+    }
+};
+
+run().catch(err => console.error(err));
+
 
 
 app.get('/', (req, res)=>{
