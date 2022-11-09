95d85f4d9201240a6694220c8c04dd8f20efc8aa
Md. Saiful Islam <saiful9122113@gmail.com>
2022-11-09 19:17:01 +0600

    checkout api added here

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -19,9 +19,17 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         const serviceCollection = client.db('serviceReviewer').collection('services');
+        
+        //Api for checkout
+        app.get('/services/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query ={_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
 
-        app.get('/limited-services', async(req, res)=>{
-            
+        //Api for limited services
+        app.get('/limited-services', async(req, res)=>{   
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.limit(3).toArray();
@@ -30,6 +38,7 @@ async function run(){
             res.send(services);
         })
 
+        //Api for all services
         app.get('/services', async(req, res)=>{
             const query = {};
             const cursor = serviceCollection.find(query);
